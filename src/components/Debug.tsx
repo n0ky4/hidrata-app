ca2d77c62c7258e5acca255349be35385a66241c
umgustavo <gugu.r.azevedo@gmail.com>
2023-05-03 16:36:09 -0300

    Add z depth

--- src/components/Debug.tsx
+++ src/components/Debug.tsx
@@ -4,7 +4,7 @@ interface DebugProps {
 
 export function Debug({ children }: DebugProps) {
     return (
-        <div className='w-full h-full fixed top-0 left-0 bg-black/50'>
+        <div className='w-full h-full fixed top-0 left-0 bg-black/50 z-50'>
             <div className='max-w-screen-md mx-auto p-4 bg-zinc-800 py-10'>{children}</div>
         </div>
     )
