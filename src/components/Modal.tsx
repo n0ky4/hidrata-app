05dcdb4e86884e80a2d90a50b747d8f732d54b80
umgustavo <gugu.r.azevedo@gmail.com>
2023-05-08 16:59:47 -0300

    Add unfinished modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,28 +1,39 @@
 import { Transition } from '@headlessui/react'
 import { X } from '@phosphor-icons/react'
-import { Fragment } from 'react'
+import React, { Fragment } from 'react'
 import { GhostButton } from './GhostButton'
 
 interface ModalProps {
     children: React.ReactNode
-    title?: string
-    description?: string
+    title?: string | React.ReactNode
+    description?: string | React.ReactNode
     show: boolean
     canClose?: boolean
     onModalClose?: () => void
 }
 
-export default function Modal({
-    children,
-    title,
-    description,
-    show,
-    onModalClose,
-    canClose = true,
-}: ModalProps) {
+type SubComponentType = {
+    type: {
+        name?: string
+    }
+}
+
+function Modal({ children, show, onModalClose, canClose = true }: ModalProps) {
     const closeModal = () => {
         if (canClose && onModalClose) onModalClose()
     }
+
+    const subComponentList = Object.keys(Modal)
+
+    const subComponents = subComponentList.map((key) => {
+        return React.Children.map(children, (child) => {
+            return (child as ).type.name === key ? child : null
+        })
+    })
+
+    const title = subComponents.find((component) => component[0].type.name === 'Title')
+    const description = subComponents.find((component) => component[0].type.name === 'Description')
+    const content = subComponents.find((component) => component[0].type.name === 'Content')
 
     return (
         <Transition.Root show={show}>
@@ -38,16 +49,15 @@ export default function Modal({
                 >
                     <div className='max-w-xl mx-auto z-10 relative p-6 rounded-xl bg-zinc-800 flex flex-col gap-4'>
                         <div className='flex items-center justify-between'>
-                            <h1 className='text-2xl font-bold'>{title}</h1>
+                            {title}
                             {canClose ? (
                                 <GhostButton onClick={() => closeModal()}>
                                     <X weight='bold' size={24} />
                                 </GhostButton>
                             ) : null}
                         </div>
-
-                        {description ? <p className='text-md'>{description}</p> : null}
-                        {children}
+                        {description}
+                        {content}
                     </div>
                 </Transition.Child>
                 <Transition.Child
@@ -68,3 +78,20 @@ export default function Modal({
         </Transition.Root>
     )
 }
+
+const Title = ({ children }: { children: React.ReactNode }) => {
+    return <h1 className='text-2xl font-bold'>{children}</h1>
+}
+Modal.Title = Title
+
+const Description = ({ children }: { children: React.ReactNode }) => {
+    return <p className='text-md'>{children}</p>
+}
+Modal.Description = Description
+
+const Content = ({ children }: { children: React.ReactNode }) => {
+    return <>{children}</>
+}
+Modal.Content = Content
+
+export default Modal
