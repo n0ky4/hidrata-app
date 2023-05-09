183543a31bbdaba7a9968029c04f1deeb07b77f4
umgustavo <gugu.r.azevedo@gmail.com>
2023-05-09 16:36:33 -0300

    Add save custom quantity front-end

--- src/components/CustomWaterIntakeModal.tsx
+++ src/components/CustomWaterIntakeModal.tsx
@@ -1,7 +1,8 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import Checkbox from './Checkbox'
 import Modal from './Modal'
 
 const QuantitySchema = z.object({
@@ -13,6 +14,7 @@ const QuantitySchema = z.object({
         .finite('Quantidade inválida')
         .int('Quantidade inválida!')
         .positive('Quantidade inválida!'),
+    name: z.string().max(50, 'Esse nome é muito grande!').optional(),
 })
 
 export type QuantityType = z.infer<typeof QuantitySchema>
@@ -37,12 +39,27 @@ export default function CustomWaterIntakeModal({
         resolver: zodResolver(QuantitySchema),
     })
 
+    const getRandomName = (exclude?: string) => {
+        const names = [
+            'Caneca que minha mãe me deu',
+            'Taça da vitória',
+            'Copo da Hello Kitty',
+            'Garrafa do timão',
+            'Copo de requeijão',
+        ]
+        if (exclude) names.splice(names.indexOf(exclude), 1)
+        return names[Math.floor(Math.random() * names.length)]
+    }
+
+    const [randomName, setRandomName] = useState(getRandomName())
+    const [checkbox, setCheckbox] = useState(false)
+
     useEffect(() => {
         if (show) {
             // I don't know why, but this is necessary to make the input focus
-            // Without this, the input will not focus
             setTimeout(() => {
                 setFocus('quantity', { shouldSelect: true })
+                setRandomName(getRandomName(randomName))
             }, 1)
         }
     }, [show])
@@ -73,6 +90,32 @@ export default function CustomWaterIntakeModal({
                             </span>
                         )}
                     </div>
+                    <Checkbox
+                        name='save-custom'
+                        checked={checkbox}
+                        onClick={(e) => setCheckbox(e as boolean)}
+                    >
+                        Salvar
+                    </Checkbox>
+                    {checkbox && (
+                        <div className='flex flex-col gap-2'>
+                            <label htmlFor='name' className='font-semibold'>
+                                Nome (opcional):
+                            </label>
+                            <input
+                                type='text'
+                                className='font-lg bg-zinc-900 border-2 border-zinc-700 rounded p-2'
+                                placeholder={randomName}
+                                {...register('name')}
+                            />
+                            {errors.name && (
+                                <span className='block font-sm text-red-500'>
+                                    {errors.name.message}
+                                </span>
+                            )}
+                        </div>
+                    )}
+
                     <div>
                         <button
                             type='submit'
