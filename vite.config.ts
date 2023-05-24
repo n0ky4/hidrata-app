d9a7b189ef372404a9f671247293d51b5091f5c2
gustavo <gugu.r.azevedo@gmail.com>
2023-05-24 15:37:34 -0300

    Get repo url from package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 import react from '@vitejs/plugin-react'
 import { execSync } from 'child_process'
+import fs from 'fs'
 import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
 
+const pkg = JSON.parse(fs.readFileSync('package.json', 'utf-8'))
 const commitHash = execSync('git rev-parse --short HEAD').toString()
 const commitDate = execSync('git log -1 --format="%ad" --date=format:"%d-%m"').toString()
+const repo = pkg?.repository
 
 // https://vitejs.dev/config/
 export default defineConfig({
     define: {
-        __REPO__: JSON.stringify('https://github.com/umgustavo/hidrata-app'),
+        __REPO__: JSON.stringify(repo),
         __COMMIT_HASH__: JSON.stringify(commitHash.trim()),
         __COMMIT_DATE__: JSON.stringify(commitDate.trim()),
     },
