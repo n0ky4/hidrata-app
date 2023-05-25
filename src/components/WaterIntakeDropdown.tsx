ae672bdf85fafb60a44cb82323b47311131a6bac
gustavo <gugu.r.azevedo@gmail.com>
2023-05-25 15:26:52 -0300

    Remove light theme classes

--- src/components/WaterIntakeDropdown.tsx
+++ src/components/WaterIntakeDropdown.tsx
@@ -47,11 +47,11 @@ function DropdownItem({
         <DropdownMenuPrimitive.Item
             className={clsx(
                 'flex cursor-default select-none items-center rounded-md px-2 py-2 text-sm outline-none',
-                'text-gray-400 focus:bg-gray-50 dark:text-zinc-500 dark:focus:bg-zinc-800'
+                'text-zinc-500 focus:bg-zinc-800'
             )}
             onClick={_handleClick}
         >
-            <div className='text-zinc-700 dark:text-zinc-300 flex items-center gap-1.5 flex-grow overflow-hidden'>
+            <div className='text-zinc-300 flex items-center gap-1.5 flex-grow overflow-hidden'>
                 {icon}
                 <span>
                     {type === 'custom'
@@ -115,7 +115,7 @@ export function WaterIntakeDropdown({ onAdd, onOpenModal, containers }: WaterInt
                     className={clsx(
                         'radix-side-top:animate-slide-up radix-side-bottom:animate-slide-down',
                         'w-48 rounded-lg px-1.5 py-1 shadow-md md:w-56',
-                        'bg-white dark:bg-zinc-900'
+                        'bg-zinc-900'
                     )}
                 >
                     {dropdownItems.map(({ type, label, icon, shortcut }) => (
