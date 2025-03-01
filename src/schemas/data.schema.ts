465316dfde58ca9f4199cb2b654bb30b4dbc522c
nokya <n0ky4@proton.me>
2025-03-01 18:29:32 -0300

    [ref] update data schema, use export

--- src/schemas/data.schema.ts
+++ src/schemas/data.schema.ts
@@ -1,25 +1,26 @@
 import { z } from 'zod'
 import { climate } from '../core/climate'
 
-const recordSchema = z.object({
+export const recordSchema = z.object({
     time: z.string(), // Data em UTC
     amount: z.number().positive(), // Quantidade deve ser um número positivo
     containerId: z.string().optional(), // Container é opcional
 })
+export type Record = z.infer<typeof recordSchema>
 
-const historyEntrySchema = z.object({
+export const historyEntrySchema = z.object({
     date: z.string().regex(/^\d{4}-\d{2}-\d{2}$/), // Valida formato YYYY-MM-DD
     climate: z.enum(Object.values(climate.conditions) as [string, ...string[]]), // Condição climática
     goal: z.number().positive(), // Meta deve ser um número positivo
     consumed: z.number().positive(), // Consumido deve ser um número positivo
     records: z.array(recordSchema), // Lista de registros
 })
+export type HistoryEntry = z.infer<typeof historyEntrySchema>
 
-const configSchema = z.object({
+export const configSchema = z.object({
     version: z.string(), // Versão do arquivo de configuração
     consumption: z.object({
         history: z.array(historyEntrySchema), // Histórico de consumo
     }),
 })
-
-export default configSchema
+export type Config = z.infer<typeof configSchema>
