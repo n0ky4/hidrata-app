b6b6d7f61deb881b47c0367f1e31f974e29529bc
nokya <n0ky4@proton.me>
2025-03-09 08:55:36 -0300

    [feat] add getContainer function

--- src/core/defaultContainers.ts
+++ src/core/defaultContainers.ts
@@ -2,4 +2,8 @@ export const defaultContainers = {
     glass: 250,
     bottle: 500,
 }
+
+export const getContainer = (container: keyof typeof defaultContainers) =>
+    defaultContainers[container]
+
 export type DefaultContainer = keyof typeof defaultContainers
