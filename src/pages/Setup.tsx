6b10bc473cfd72524f1de5846e5ac5be36355765
nokya <n0ky4@proton.me>
2025-03-12 18:13:49 -0300

    [ref] add debug log

--- src/pages/Setup.tsx
+++ src/pages/Setup.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router'
 import { FirstUseScreen } from '../components/FirstUseScreen'
 import { useInitHandler } from '../core/initHandler'
+import { log } from '../util/logger'
 
 export default function SetupPage() {
     const { setupData } = useInitHandler()
@@ -13,6 +14,7 @@ export default function SetupPage() {
     useEffect(() => {
         const isFirstUse = setupData()
         if (!isFirstUse) {
+            log.info('first use detected, redirecting to /')
             navigate('/')
             return
         }
